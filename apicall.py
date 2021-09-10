6cd9ff1458b6d532ffdcc747b9dc45869cea56d1
Divyanshu Deoli <60615629+divyanshudeoli@users.noreply.github.com>
2021-09-10 13:49:49 +0530

    Input through HTML Form

--- apicall.py
+++ apicall.py
@@ -1,2 +1,14 @@
-import requests
-requests.get()
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def Hello():
+	return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():    
+    return request.form.get('name')
+
+if __name__=='__main__':
+	app.run(debug=True,host='127.0.0.1',port=5000)
